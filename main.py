51b1ef45cd435f04ca3c6397e0c70e36bfe03699
tejashyamehta <116365636+tejashyamehta@users.noreply.github.com>
2025-03-10 23:15:24 +0530

    Error Handling added

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ alphabet_dataframe = pandas.DataFrame(data)
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a Word\n").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a Word\n").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except:
+        print("Kindly enter a word only")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
